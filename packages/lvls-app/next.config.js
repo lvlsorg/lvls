a6274a8f8c7a464a8d2b67a24557dbc35f1b613e
Zane Starr <zcstarr@gmail.com>
2023-04-11 14:06:17 -0700

    feat: add support for forge

---
+++ packages/lvls-app/next.config.js
@@ -0,0 +1,14 @@
+module.exports =  {
+  experimental: {
+    appDir: true,
+    externalDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+    ],
+  },
+}
