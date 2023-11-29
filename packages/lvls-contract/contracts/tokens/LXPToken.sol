4b9c6b1d329b52715c8ada01520d86283b7e72c5
Zane Starr <zcstarr@gmail.com>
2023-11-28 20:05:11 -0800

    fix: wip translate to using non diamond facets

--- packages/lvls-contract/contracts/tokens/LXPToken.sol
+++ packages/lvls-contract/contracts/tokens/LXPToken.sol
@@ -1,6 +1,13 @@
-import {LSP7DigitalAsset} from "@lukso/lsp-smart-contracts/contracts/LSP7DigitalAsset/LSP7DigitalAsset.sol"
+import {LSP7DigitalAsset} from "@lukso/lsp-smart-contracts/contracts/LSP7DigitalAsset/LSP7DigitalAsset.sol";
 
-contract LXPFacet is LSP7DigitalAssetFacet {
+contract LXPFacet is LSP7DigitalAsset {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address newOwner_,
+        bool isNonDivisible_
+    ) LSP7DigitalAsset(name_, symbol_, newOwner_, isNonDivisible_) {}
+
     function transfer(address from, address to, uint256 amount, bool allowNonLSPRecipient, bytes memory data) public virtual override {
         // Transfer should revert as this is Soul bound
         revert("soulbound non transferrable");
